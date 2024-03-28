4da73df279c7751ad987c755a8d47c2642433053
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-28 13:03:15 +0800

    Save

--- Introduction.py
+++ Introduction.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 import torch
 
 # Model and tokenizer names (you can choose a different model from Hugging Face)
-model_name = "distilbert/distilbert-base-cased-distilled-squad"
+model_name = "google/gemma-7b-it"
 tokenizer_name = model_name
 
 access_token = st.secrets["API_key"]
