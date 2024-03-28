56a338424e3d4062c8f67cd378ba0cbb270c4a97
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-28 09:27:58 +0800

    Save

--- Introduction.py
+++ Introduction.py
@@ -35,12 +35,14 @@ def answer_question(document, question):
   return {"answer": answer[0], "start": answer_start, "end": answer_end}
 
 def app():
-  st.write(access_token)
+
   # Model and tokenizer names (you can choose a different model from Hugging Face)
   model_name = "distilbert-base-cased-squad2"
   tokenizer_name = model_name
 
   access_token = st.secrets["API_key"]
+  st.write(access_token)
+  
   # Load pre-trained model and tokenizer
   tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, token=access_token)
   model = AutoModelForQuestionAnswering.from_pretrained(model_name, token=access_token)
