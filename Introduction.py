e48e170584e0057ab9ebb19a1646423e68c4a9b0
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-28 10:33:48 +0800

    Save

--- Introduction.py
+++ Introduction.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 import torch
 
 # Model and tokenizer names (you can choose a different model from Hugging Face)
-model_name = "rajpurkar/squad_v2"
+model_name = "distilbert/distilbert-base-cased-distilled-squad"
 tokenizer_name = model_name
 
 access_token = st.secrets["API_key"]
